c1e57263b8f98a2038f4e614ef0618cd1d2fea1d
KZhukow <kosty.zhukov.2011@gmail.com>
2022-03-14 21:01:51 +0300

    feat: add some solutions (ST) Delete digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -12,16 +12,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function deleteDigit(n) {
-  let arr = [];
+  // let arr = [];
+  // let num = Array.from(String(n), Number);
+  // for (let i = 0; i < num.length; i++) {
+  //   let s = '';
+  //   for (let j = 0; j < num.length; j++) {
+  //     if (i != j) s += num[j];
+  //   }
+  //   arr.push(+s);
+  // }
+  // return arr.sort( (a, b) => b - a)[0];
+
+  // let num = Array.from(String(n), Number);
+  // for (let i = 0; i < num.length; i++) {
+  //   if (i == num.length - 1) {
+  //     num.pop();
+  //   } else if (num[i] < num[i+1]) {
+  //     num.splice(i, 1);
+  //     break;
+  //   }    
+  // }
+  // return +num.join('');
+
+  // let k = 0;
+  // let num = Array.from(String(n), Number);
+  // for (let i = 0; i < num.length; i++) {
+  //   k = Math.max(k, +num.filter((a, j) => j != i).join(''));
+  // }
+  // return k; 
+
   let num = Array.from(String(n), Number);
-  for (let i = 0; i < num.length; i++) {
-    let s = '';
-    for (let j = 0; j < num.length; j++) {
-      if (i != j) s += num[j];
-    }
-    arr.push(+s);
-  }
-  return arr.sort( (a, b) => b - a)[0];
+  return num.reduce((k, n, i) => Math.max(k, +num.filter((a, j) => j != i).join('')), 0);
 }
 
 module.exports = {
